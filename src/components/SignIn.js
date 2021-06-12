46faa0bc2fd9bac7f9a8f78ba9e82c1a9e47fe35
“SakodaShiho” <sawadasakura322@gmail.com>
2021-06-12 17:03:10 +0900

    save name in japanese

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -48,7 +48,8 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState("");
-  console.log({ disabled, string });
+  const [isComposed, setIsComposed] = useState(false);
+  console.log({ disabled, string, isComposed });
   useEffect(() => {
     const disabled = string === "";
     setDisabled(disabled);
@@ -73,11 +74,14 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
+              if (isComposed) return;
               if (e.key === "Enter") {
                 setName(e.target.value);
                 e.preventDefault();
               }
             }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
